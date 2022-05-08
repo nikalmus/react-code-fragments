ab906336b2a6bbe61104e07ad3aeb1756c62bf63
nickrally <nickrally2009@gmail.com>
2022-05-08 10:00:30 -0600

    toggled details

--- src/pages/TodosApi/components/Todo.js
+++ src/pages/TodosApi/components/Todo.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
 
 const Todo = ({ item }) => {
+  const [expanded, setExpanded] = useState(false);
   return (
     <>
       <div className="item">{item.title}</div>
+      <div
+        className="expand-collapse"
+        onClick={() => setExpanded((prev) => !prev)}
+      >
+        {expanded ? "-" : "+"}
+      </div>
+      <div className="details">
+        {expanded
+          ? Object.entries(item).map(([key, value]) => {
+              return key !== "title" ? <p>{`${key}:${value}`}</p> : "";
+            })
+          : ""}
+      </div>
     </>
   );
 };
