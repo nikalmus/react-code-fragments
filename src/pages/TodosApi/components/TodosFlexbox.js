0c9ca3c2c86d5e737532e4a008f0d2e39cc1db71
nickrally <nickrally2009@gmail.com>
2022-05-13 14:22:01 -0600

    flexbox looks identical to grid

--- src/pages/TodosApi/components/TodosFlexbox.js
+++ src/pages/TodosApi/components/TodosFlexbox.js
@@ -1,4 +1,22 @@
+import GridItemTitle from "./GridItemTitle";
+import GridItemStatus from "./GridItemStatus";
+import "./TodosGrid.css";
 const TodosFlexbox = ({ data }) => {
-  return <div>flexbox</div>;
+  return (
+    <div class="wrapper">
+      <span>
+        <strong>Title</strong>
+      </span>
+      <span>
+        <strong>Completed</strong>
+      </span>
+      {data.map((item) => (
+        <>
+          <GridItemTitle className="item" id={item.id} title={item.title} />
+          <GridItemStatus className="item" completed={item.completed} />
+        </>
+      ))}
+    </div>
+  );
 };
 export default TodosFlexbox;
