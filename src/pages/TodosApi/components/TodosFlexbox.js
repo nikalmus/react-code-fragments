f42eddd55b7e1c68c1f62688d9f398a7317097a7
nickrally <nickrally2009@gmail.com>
2022-05-15 14:01:19 -0600

    rows of two cells

--- src/pages/TodosApi/components/TodosFlexbox.js
+++ src/pages/TodosApi/components/TodosFlexbox.js
@@ -4,27 +4,17 @@ const TodosFlexbox = ({ data }) => {
   return (
     <div class="wrapper">
       <div className="row">
-        <div className="column">
+        <div className="name-cell">
           <strong>Title</strong>
         </div>
-        <div className="column">
+        <div className="completed-cell">
           <strong>Completed</strong>
         </div>
       </div>
       {data.map((item) => (
         <div class="row">
-          <div class="column">
-            <div class="name-column">{`ID${item.id}: ${item.title}`}</div>
-          </div>
-          <div class="column">
-            <div
-              class={`completed-column ${
-                item.completed ? "completed" : "not-completed"
-              }`}
-            >
-              {item.completed.toString()}
-            </div>
-          </div>
+          <div class="name-cell">{`ID${item.id}: ${item.title}`}</div>
+          <div class="completed-cell">{item.completed.toString()}</div>
         </div>
       ))}
     </div>
