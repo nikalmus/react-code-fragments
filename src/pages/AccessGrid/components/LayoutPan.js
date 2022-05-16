c151dfec988666959360c64a2c1031f59c1ce157
nickrally <nickrally2009@gmail.com>
2022-05-15 22:12:10 -0600

    Todo flexbox (#3)
    * started flexbox
    
    * flexbox looks identical to grid
    
    * flexbox grid
    
    * rows of two cells
    
    * flexbox vs grid

---
+++ src/pages/AccessGrid/components/LayoutPan.js
@@ -0,0 +1,14 @@
+import SimpleGrid from "./SimpleGrid";
+import SeriousGrid from "./SeriousGrid";
+import "./LayoutPan.css";
+
+const LayoutPan = () => {
+  return (
+    <>
+      <SimpleGrid />
+      <SeriousGrid />
+    </>
+  );
+};
+
+export default LayoutPan;
