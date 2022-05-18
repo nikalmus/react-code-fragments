a7f6e8cd4d6f7179179a81908b77744e65511700
nickrally <nickrally2009@gmail.com>
2022-05-17 22:33:52 -0600

    modal with ref

--- src/pages/List/components/List.js
+++ src/pages/List/components/List.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import ItemModal from "./ItemModal";
+import "./List.css";
 
 const List = () => {
   const [list, setList] = useState([]);
   const [value, setValue] = useState("");
+  const [itemOpen, setItemOpen] = useState(null);
+
+  const modalRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,6 +18,21 @@ const List = () => {
     setList(list.filter((el) => el !== item));
   };
 
+  const openModal = (item) => {
+    setItemOpen(item);
+  };
+
+  useEffect(() => {
+    const onClickOutsideModal = (e) => {
+      itemOpen &&
+        modalRef.current &&
+        !modalRef.current.contains(e.target) &&
+        setItemOpen(null);
+    };
+    document.addEventListener("mousedown", onClickOutsideModal);
+    return () => document.removeEventListener("mousedown", onClickOutsideModal);
+  }, [itemOpen]);
+
   return (
     <>
       <form onSubmit={(e) => handleSubmit(e)}>
@@ -21,13 +41,21 @@ const List = () => {
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
-        <input type="submit" />
+        <input type="submit" disabled={!value} />
       </form>
       {list.map((item, i) => (
-        <li key={i} onClick={() => handleDelete(item)}>
-          {item}
-        </li>
+        <div className="box" key={i}>
+          <div className="text" onClick={() => openModal(item)}>
+            {item}
+          </div>
+          <button className="btn" onClick={() => handleDelete(item)}>
+            x
+          </button>
+        </div>
       ))}
+      {itemOpen && (
+        <ItemModal item={itemOpen} setOpen={setItemOpen} modalRef={modalRef} />
+      )}
     </>
   );
 };
